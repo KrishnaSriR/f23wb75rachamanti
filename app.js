9bcfc112822c7633b2128273f50e27acc352afbd
KrishnaSriR <KrishnaSriR@users.noreply.github.com>
2023-10-31 14:39:53 -0500

    Part 9 added choose endpoint

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var educationRouter = require('./routes/education');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/education', educationRouter);
 app.use('/board', boardRouter);
+app.use('/choose',chooseRouter);
 
 
 
